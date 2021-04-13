d67c4311ae4b0999e8d4244ec9b32da9862c0168
Thanat <bztzn512@gmail.com>
2021-04-13 17:55:20 -0500

    secondSolution

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const nums = [2, 7 ,11 ,15];
-const target = 9;
+const target = 13;
 
 const firstTwoSum = function (nums, target) {
-    
+    const map = {};
+    for (let i = 0; i < nums.length; i++) {
+         const value = nums[i];
+         const comp = target - nums[i]; 
+
+         if (map[comp] !== undefined) {
+             return [map[comp], i];
+         } else {
+             map[value] = i;
+         }
+    }
 }
 
-
-
+console.log(firstTwoSum(nums, target));
 ////////////////////////////////////
-let twoSum = function (nums, target) {
+ let twoSum = function (nums, target) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
              if (nums[i] + nums[j] === target) {
@@ -17,5 +26,3 @@ let twoSum = function (nums, target) {
         }
     }
 };
-
-console.log(twoSum(nums, target));
