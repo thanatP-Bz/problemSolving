94ca922b5a4d7f2d91e852304f8e481abea1e40a
Thanat <bztzn512@gmail.com>
2021-04-14 09:49:16 -0500

    ThirdSolution

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
-const nums = [2, 7 ,11 ,15];
-const target = 13;
 
-
-
-
-
-const firstTwoSum = function (nums, target) {
+////////////////////////////////////
+/* const firstTwoSum = function (nums, target) {
     const map = {};
     for (let i = 0; i < nums.length; i++) {
          const value = nums[i];
          const comp = target - nums[i]; 
-
+        
          if (map[comp] !== undefined) {
              return [map[comp], i];
          } else {
              map[value] = i;
          }
     }
-}
+}; */
 
-console.log(firstTwoSum(nums, target));
+
 ////////////////////////////////////
- let twoSum = function (nums, target) {
+/*  let twoSum = function (nums, target) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
              if (nums[i] + nums[j] === target) {
@@ -29,4 +24,45 @@ console.log(firstTwoSum(nums, target));
              }
         }
     }
+}; */
+/////////////////////////////////////
+
+/* const numbers = [2, 4, 5, 17];
+const targetTwo = 9;
+
+
+const testNum = (num1, target) => {
+    const map = {};
+for (let i = 0; i < num1.length; i++) {
+    const value = num1[i];
+    const comp = target - value;
+    if(map[comp] !== undefined) {
+        return [map[comp], i];
+    } else {
+        map[value] = i;
+    }
+  }
 };
+ */
+
+///////////////////////////////
+
+const SecondTwoSum = (nums, target) => {
+    const comp = new Map();
+    const len = nums.length;
+
+    for (let i = 0; i < len; i++) {
+        if (comp[nums[i]] >= 0) {
+            return [comp[nums[i]], i];
+        }
+        comp[target - nums[i]] = i;
+    }
+
+    return [];
+    
+};
+
+const nums = [2, 7 ,11 ,15];
+const target = 9;
+
+console.log(SecondTwoSum(nums, target));
