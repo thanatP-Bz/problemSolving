76f72086538c1f7225b625e1ff30ea59b5df9734
Thanat <bztzn512@gmail.com>
2023-09-15 20:07:07 -0400

    addPrintList

--- dataStructure.js
+++ dataStructure.js
@@ -47,10 +47,23 @@ class LinkedList {
     this.head = newNode;
     return this;
   }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  insert(index, value) {}
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(4);
+myLinkedList.printList();
 console.log(myLinkedList);
