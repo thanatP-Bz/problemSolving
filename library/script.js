562707bb2ee0535e6cae008ecdb0cc239518298b
Thanat <bztzn512@gmail.com>
2024-01-29 20:41:59 -0500

    addCard

--- library/script.js
+++ library/script.js
@@ -1,4 +1,10 @@
+let formSumit = document.getElementById("new-book-form");
+const addBtn = document.querySelector(".add-btn");
+const cancelBtn = document.querySelector(".cancel");
+let newBookForm = document.querySelector(".form-control");
+
 const myLibrary = [];
+console.log(myLibrary);
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -6,18 +12,30 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
 }
+function addBookToLibrary() {
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").checked;
+  let newBook = new Book(title, author, pages, read);
 
-let title = document.getElementById("title").value;
-let author = document.getElementById("author").value;
-let pages = document.getElementById("pages").value;
-let read = document.getElementById("read").checked;
-let newBook = new Book(title, author, pages, read);
+  newBookForm.style.display = "none";
 
-function addBookToLibrary(title, author, pages, read) {}
+  myLibrary.push(newBook);
+  render();
+}
 
-const addBtn = document.querySelector(".add-btn");
-const cancelBtn = document.querySelector(".cancel");
-let newBookForm = document.querySelector(".form-control");
+function render() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    console.log(myLibrary[i]);
+  }
+}
+
+formSumit.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  addBookToLibrary();
+});
 
 addBtn.addEventListener("click", () => {
   newBookForm.style.display = "block";
